5e2d6726b78489ccb943117244b60bae5c78668d
Dharanish3 <dharanishsk9698@gmail.com>
2024-03-05 18:12:52 +0530

    Dashboard Update

--- src/AdminPages/Dashboard.jsx
+++ src/AdminPages/Dashboard.jsx
@@ -2,13 +2,41 @@ import React from 'react'
 import './index.css'
 import Topbar from './Topbar'
 import Sidebar from './Sidebar'
+import { Link } from 'react-router-dom'
 
 function Dashboard() {
   
   return (
    <>
     <Topbar/>
-    <Sidebar/>
+    <Sidebar>
+          <div className="container-fluid px-4">
+                        <h1 className="mt-4">Dashboard</h1>
+                        <ol className="breadcrumb mb-4">
+                            <li className="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                        <div className="row">
+                            <div className="col-xl-6 col-md-6">
+                                <div className="card bg-success text-white mb-4">
+                                    <div className="card-body">All Movies</div>
+                                    <div className="card-footer d-flex align-items-center justify-content-between">
+                                        <Link className="small text-white stretched-link" to="/admin/movieslist">View List</Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-6 col-md-6">
+                                <div className="card bg-info text-white mb-4">
+                                    <div className="card-body">Add Movies</div>
+                                    <div className="card-footer d-flex align-items-center justify-content-between">
+                                        <Link className="small text-white stretched-link" to="/admin/movie-create">Add</Link>
+                                      
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        </div>
+    </Sidebar>
    </>
   )
 }
